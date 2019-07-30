88a6c3ff3f23404146394e96f9cf6cb9fd22bbd9
Gabriele Capparella <gabriele.capparella@gmail.com>
2019-07-30 22:38:14 +0200

    Done web backend and class DC_Motor

--- main.py
+++ main.py
@@ -1,18 +1,79 @@
 #!/usr/bin/python3
 
 from flask import Flask, render_template, request
+import RPi.GPIO as GPIO
+import signal
 
 master = Flask(__name__)
+pins = {
+	'en1': 16,
+	'in1': 20,
+	'in2': 21,
+	'en2': 14,
+	'in3': 15,
+	'in4': 18	
+}
+
+class DC_Motor:
+	def __init__(self, en_pin, fwd_pin, rev_pin):
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(en_pin, GPIO.OUT)
+		GPIO.setup(fwd_pin, GPIO.OUT)
+		GPIO.setup(rev_pin, GPIO.OUT)
+		self.fwd = fwd_pin
+		self.rev = rev_pin
+		self.en = en_pin
+		self.speed_ctrl = GPIO.PWM(en_pin, 100)
+		self.speed_ctrl.start(0)
+		GPIO.output(en_pin, True)
+
+	# speed should be in [-100, 100]
+	# positive value means forward, negative means reverse
+	def set_state(self, speed: float):
+		print('set_state', speed)
+		if abs(speed)<15: speed = 0 # REMOVE BEFORE FLIGHT
+		self.speed_ctrl.ChangeDutyCycle(abs(speed))
+		GPIO.output(self.fwd, speed>0)
+		GPIO.output(self.rev, speed<0)
+
 
 @master.route('/')
 def index():
 	return render_template('index.html')
 
+
 @master.route('/move')
 def move():
-	x = request.args['x']
-	y = request.args['y']
-	print(x, y)
+	x = float(request.args['x'])
+	y = float(request.args['y'])
+	print('args:', x, y)
+
+	if y==0:
+		master.motor_l.set_state(x)
+		master.motor_r.set_state(-x)
+	else:
+		slow = y-((abs(x*y)/100)*(y/abs(y))) # magic
+		if x>0:
+			master.motor_l.set_state(y)
+			master.motor_r.set_state(slow)
+		else:
+			master.motor_l.set_state(slow)
+			master.motor_r.set_state(y)
+
 	return 'ok'
 
+
+def motors_setup():
+	master.motor_l = DC_Motor(pins['en1'], pins['in1'], pins['in2'])
+	master.motor_r = DC_Motor(pins['en2'], pins['in3'], pins['in4'])
+
+
+def teardown_handler(signal, frame):
+	GPIO.cleanup()
+	raise SystemExit
+
+
+motors_setup()
+
+signal.signal(signal.SIGINT, teardown_handler)
 master.run('0.0.0.0', 8080)
--- static/controls.js
+++ static/controls.js
@@ -3,23 +3,26 @@ document.onselectstart = function() {
 }
 
 $(document).ready(function(){
+	var following_mouse = false;
 	var wheel_offset = $("#wheel").offset();
-	var current_x = 150;
-	var current_y = 150;
-	
-	$("#dot").css("margin-left", 150-11+"px");
-	$("#dot").css("margin-top", 150-11+"px");
+	var wheel_size = 200;
+	var current_x = wheel_size/2;
+	var current_y = wheel_size/2
+	var last_x = wheel_size/2;
+	var last_y = wheel_size/2;
 
 	$("#wheel").mousedown(function() {
+		following_mouse = true;
 		$(document).mousemove(function(e) {
-			rel_x = parseInt(e.pageX-wheel_offset.left);
-			rel_y = parseInt(e.pageY-wheel_offset.top);
+			rel_x = parseFloat(e.pageX-wheel_offset.left);
+			rel_y = parseFloat(e.pageY-wheel_offset.top);
 			move(rel_x, rel_y);
 		});
 	});
 
 	$(document).mouseup(function() {
 		$(document).off("mousemove");
+		move(wheel_size/2, wheel_size/2);
 	});
 
 	$(document).keyup(function(e){
@@ -46,18 +49,27 @@ $(document).ready(function(){
 	function move(x, y) {
 		if (x<0) x=0;
 		if (y<0) y=0;
-		if (x>300) x=300;
-		if (y>300) y=300;
+		if (x>wheel_size) x=wheel_size;
+		if (y>wheel_size) y=wheel_size;
 
-		$.ajax({
-			url:'/move', 
-			data:{
-				x: x-150,
-				y: y-150
-			},
-		});
 		current_x = x;
 		current_y = y;
+
+		diff = Math.abs(current_x-last_x)+Math.abs(current_y-last_y)
+
+		// I only send the request if the dot has been moved by at least 5px
+		if (diff>=5) { 
+			$.ajax({
+				url:'/move', 
+				data:{
+					x: x-wheel_size/2,
+					y: -y+wheel_size/2
+				},
+			});
+			last_x = x;
+			last_y = y;
+		}
+
 		$("#dot").css("margin-left", x-11+"px");
 		$("#dot").css("margin-top", y-11+"px");
 	}
